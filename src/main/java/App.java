e6ae6261a288dc26d684916a5d0fa7f215023f3f
Daren Schaad <darenschaad@gmail.com>
2016-02-12 10:45:19 -0800

    create a functioning website.  At this point the website is acceptable for the code review

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,12 +8,23 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
+    String form = "templates/home.vtl";
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/home.vtl");
-      return new ModelAndView(model, layout)
-    }, new VelocityTemplateEngine())
+      model.put("template", form);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/puzzle", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String inputSentence = request.queryParams("inputSentence");
+      String wordPuzzle = replaceVowels(inputSentence);
+      model.put("wordPuzzle", wordPuzzle);
+      model.put("template", "templates/puzzle.vtl");
+      model.put("form", form);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
   public static String replaceVowels(String inputSentence) {
